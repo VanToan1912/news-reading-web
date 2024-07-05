8f004c27a0c556c7bc1c26a0ef24a3bdb6e83f1d
VanToan1912 <21130572@st.hcmuaf.edu.vn>
2024-07-05 17:15:58 +0700

    doc rss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Feed from "./Feed";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  console.log(articles);
+  const getArticles = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/");
+      setArticles(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Báo Tuổi Trẻ RSS</h1>
+      <div>
+        {articles.map((items, i) =>
+          <Feed
+            key={i}
+            title={items.item.title}
+            link={items.item.link}
+            date={items.item.pubDate}
+          />)}
+      </div>
+    </>
   );
 }
 
