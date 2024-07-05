00146225506f6b2e6e7274efa7c2da989e4664d6
TAT Team <21130577@st.hcmuaf.edu.vn>
2024-07-05 18:47:29 +0700

    Đọc rss từ HTTP Link

---
+++ rss-feed/src/components/Feed.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import parse, { domToReact } from 'html-react-parser';
+
+const Feed = ({ content, contentSnippet, title, link, date }) => {
+  let formatted = { day: "numeric", month: "long", year: "numeric" };
+  let articleDate = new Date(date).toLocaleDateString("en-GB", formatted);
+
+  const options = {
+    replace: ({ name, attribs, children }) => {
+      if (!attribs) return;
+      if (name === 'a') {
+        return (
+          <a href={attribs.href} target="_blank" rel="noopener noreferrer">
+            {domToReact(children, options)}
+          </a>
+        );
+      }
+      if (name === 'img') {
+        return <img src={attribs.src} alt={attribs.alt || 'image'} />;
+      }
+    },
+  };
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      <div>{parse(content, options)}</div>
+      <p>{articleDate}</p>
+      <a href={link} target="_blank" rel="noopener noreferrer">{link}</a>
+    </div>
+  );
+};
+
+export default Feed;
