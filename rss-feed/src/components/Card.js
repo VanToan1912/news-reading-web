bac164698873c8daa40a6b512e0f33eca61239ac
futolatoine <21130567@st.hcmuaf.edu.vn>
2024-07-09 11:35:12 +0700

    them card

---
+++ rss-feed/src/components/Card.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import parse, { domToReact } from 'html-react-parser';
+import '../assets/styles/card.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+ // Trích xuất URL hình ảnh từ content
+ const extractImageUrl = (content) => {
+    const imgTag = /<img.*?src="(.*?)"/;
+    const match = content.match(imgTag);
+    return match ? match[1] : '';
+  };
+
+  // hàm để cất nội dung để hiển thị tối đa maxLength ký tự
+  const truncate = (content, maxLength) => {
+    if (content.length > maxLength) {
+      return content.substring(0, maxLength) + "...";
+    } else {
+      return content;
+    }
+  };
+
+
+const ArticleCard = ({ content, contentSnippet, title, link, date }) => {
+//   let formatted = { day: "numeric", month: "long", year: "numeric" };
+//   let articleDate = new Date(date).toLocaleDateString("en-GB", formatted);
+
+  const options = {
+    replace: ({ name, attribs, children }) => {
+      if (!attribs) return;
+      if (name === 'a') {
+        return (
+          <a href={attribs.href} target="_blank" rel="noopener noreferrer">
+            {domToReact(children, options)}
+          </a>
+        );
+      }
+      if (name === 'img') {
+        return <img src={attribs.src} alt={attribs.alt || 'image'} />;
+      }
+    },
+  };
+
+      const imageUrl = extractImageUrl(content);
+ 
+  return ( 
+    <div className="article-card row">
+        <div className = "col thumnails">
+             <a href={link} target="_blank" rel="noopener noreferrer" className = "title-article">
+             {imageUrl && <img src={imageUrl} alt="article" className="img-fluid img-resize" />}
+             </a>
+           
+        </div>
+
+        <div className = "col box-text">
+            <h3 className = "box-title-text">
+                <a href={link} target="_blank" rel="noopener noreferrer" className = "title-article">{truncate(title,20)}</a>
+            </h3>
+             <div className = "content-s">
+                 <div className="content-snippet">
+                 <p >
+                 {parse(truncate(contentSnippet, 150), options)}
+                 </p>
+                 </div>
+                
+             </div>
+             
+
+        </div>
+      
+    
+     
+    </div>
+  );
+};
+
+
+export const MainCard = ({ content, title, link}) => {
+
+    const imageUrl = extractImageUrl(content);
+
+    return ( 
+        <div className="article-card col">
+            <div className = "col-8 thumnails">
+                 <a href={link} target="_blank" rel="noopener noreferrer" className = "title-article">
+                 {imageUrl && <img src={imageUrl} alt="article" className="img-fluid img-resize" />}
+                 </a>
+               
+            </div>
+    
+            <div className = "col-4">
+                <h3 className = "box-title-text">
+                    <a href={link} target="_blank" rel="noopener noreferrer" className = "title-article">{truncate(title,20)}</a>
+                </h3>
+    
+            </div>
+          
+        
+         
+        </div>
+      );
+}
+export default ArticleCard;
--- rss-feed/src/components/Header.js
+++ rss-feed/src/components/Header.js
@@ -1,15 +1,15 @@
-
 import '../css/Header.css'
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass, faUser, faHouse } from '@fortawesome/free-solid-svg-icons'
 
+
 const Header = () => {
 
     return (
         <div className="Header">
-            <header className="navbar">
-                <div className="navbar-top">
+            <header className="navbar-head">
+                <div className="navbar-top-head">
                     <img src="https://static-tuoitre.tuoitre.vn/tuoitre/web_images/logottonew.svg" alt="Tuoi Tre Online" className="logo" />
                     <div className="navbar-top-links">
                         <a href="#podcast">Podcast</a>
@@ -21,7 +21,7 @@ const Header = () => {
                         <button className="btn-subscribe">Đăng ký Tuổi Trẻ Sao</button>
                     </div>
                 </div>
-                <div className="navbar-bottom">
+                <div className="navbar-bottom-head">
                     <a href="#trang-chu" className="home-btn"><FontAwesomeIcon icon={faHouse} color='#ff0000' fontSize={20} /></a>
                     <a href="#video">VIDEO</a>
                     <a href="#thoi-su">THỜI SỰ</a>
